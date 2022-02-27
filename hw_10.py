d0fc0397d5d28ec898f2fe4d32df6e421096702a
RKorets <93273641+RKorets@users.noreply.github.com>
2022-02-27 20:10:38 +0200

    hw_10.py

--- hw_10.py
+++ hw_10.py
@@ -1,81 +1,44 @@
+
 from collections import UserDict
 
+
+class Field:
+    def __init__(self, value) -> None:
+        self.value = value
+
+    def __repr__(self) -> str:
+        return self.value
+
+    def __str__(self) -> str:
+        return self.value
+
+
+class Phone(Field):
+    pass
+
+
+class Name(Field):
+    pass
+
+
 class Record:
+    def __init__(self, name: Name, phone: Phone) -> None:
+        self.name = name
+        self.phones = []  # спиисок экземпляр класа Phone
+        self.phones.append(phone)
 
-    def __init__(self, name, phone) -> None:
-        value = Name(name) #экземпляр класа Name
-        self.name = value
-        telephone = Phone(phone) #экземпляр класа Phone
-        self.phones = [] #спиисок экземпляр класа Phone
-        self.phones.append(telephone)
+    def add_phone(self, phone: Phone):
+        self.phones.append(phone)
 
-    def add_phone(self,phone):
-        add_ph = Phone(phone)
-        self.phones.append(add_ph)
-
-    def change_phone(self,phone,new_phone):
-        for el in self.phones:
-            if phone in el.phones:
-                 el.phones = [new_phone]
-                 return
-        print("Phone number not exist! Try again")
-
-    def delete_phone(self,phone):
-        for el in self.phones:
-            if phone in el.phones:
-                el.phones.remove(phone)
-                return
-        print("Phone number not exist! Try again")
+    def __str__(self) -> str:
+        return f'name: {self.name}, phones: {", ".join([str(p) for p in self.phones])}'
 
 
 class AddressBook(UserDict):
-
-    def add_record(self, obj):
-        if isinstance(obj, Record):
-            self.data[obj.name.value] = obj
+    def add_record(self, *args):
+        for item in args:
+            self.data[item.name] = item
 
     def __str__(self) -> str:
         result = "\n".join([str(v) for v in self.data.values()])
         return result
-
-    def show_all_contact(self):
-        print("Имя           Номер телефона")
-        for name, phone in self.data.items():
-            phone_number = [ph.phones[0] for ph in phone.phones if len(ph.phones)!=0]
-            print('{:<10} - {}'.format(name,phone_number))
-
-
-class Field:
-    pass
-
-
-class Phone(Field):
-
-    def __init__(self,phone=None):
-        self.phones = [phone]
-
-
-class Name(Field):
-
-    def __init__(self,name):
-        self.value = name
-
-
-
-
-
-ab = AddressBook()
-record = Record('Bill', '23456777')
-ab.add_record(record)
-
-record.add_phone("09543242342")
-record.add_phone("0961019231")
-record.change_phone("0961019231","000007")
-record.delete_phone("23456777")
-record.delete_phone("0969019233")
-
-print(record.phones)
-print(record.name.value)
-print(ab.data['Bill'].phones)
-ab.show_all_contact()
-
